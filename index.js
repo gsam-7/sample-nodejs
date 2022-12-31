598d452f89e90ec854f740662d17e0f831d772f2
Samridh Garg <68631593+gsam-7@users.noreply.github.com>
2022-12-31 10:22:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+npm i puppeteer mocha selenium-webdriver chai
+const puppeteer = require('puppeteer');
 
-var LoremIpsum = require('lorem-ipsum').LoremIpsum;
+let browser;
 
-var lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
+let val = 10;
+// enter the amount of fake votes you want on every run
 
-app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+for (let i = 0; i < val; i++) {
+  (async () => {
+    if (!browser) {
+      browser = await puppeteer.launch({ headless: false });
+    }
+    const page = await browser.newPage();
+    await page.goto('https://ngx.me/4VNu6LV.h?ngxItemID=1980e0a5f5eacce9996483416dbdae7&s=RyJjIjoiZGpqeXU6QkJjdGUubGJtam1hcy56YnRCaWNCcj10RldTdDIxUFpCZGh3c3hxbXZqLnJ5ZSIsInoiOiJ1bXR0cW5sIlQ%3D');
+    await page.evaluate(() => {
+      $('.xDetailContainer .xButton')[3].click()
+    });
+    await setTimeout(async () => {
+      await browser.close();
+    }, 1000);
+  })();
+}
